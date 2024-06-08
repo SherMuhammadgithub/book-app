4e6f6d12cf90ab4838dcdcff629da499c16aa00c
SherMuhammadgithub <muhammadiqbalshermuhammad@gmail.com>
2024-06-08 12:32:32 +0500

    updated code

--- mini-book-app/server/index.js
+++ mini-book-app/server/index.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const router = require("./routes/Book-route");
 const router2 = require("./routes/User-route");
 
 // Middleware
 app.use(express.json()); // To parse JSON request bodies
-app.use(cors({
+app.use(
+  cors({
     origin: "https://book-app-frontend-virid.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/books", router); // localhost:5000/books
 app.use("/user", router2); // localhost:5000/user
 
-mongoose.connect(
+mongoose
+  .connect(
     "mongodb+srv://foodApp123:foodApp123@cluster0.txvgoyc.mongodb.net/bookStore?retryWrites=true&w=majority"
-)
-    .then(() => {
-        console.log("Connected to Database");
-        app.listen(5000, () => {
-            console.log("Server is running on port 5000");
-        });
-    })
-    .catch((err) => console.log("Error connecting to Database", err));
+  )
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch((err) => console.log("Error connecting to Database", err));
+
 app.get("/", (req, res) => {
-res.json("Hello");
+  res.json("Hello");
 });
