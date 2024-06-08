aebd94794bed5688e8d2c29e413ad91ed9b4490b
Sher Muhamad Iqbal <136792000+SherMuhammadgithub@users.noreply.github.com>
2024-06-08 12:14:15 +0500

    Update index.js

--- mini-book-app/server/index.js
+++ mini-book-app/server/index.js
@@ -4,22 +4,26 @@ const app = express();
 const cors = require('cors');
 const router = require("./routes/Book-route");
 const router2 = require("./routes/User-route");
-// MiddleWare 
-app.use(express.json()); // error solution for converting data 
+
+// Middleware
+app.use(express.json()); // To parse JSON request bodies
 app.use(cors({
-     origin:"https://book-app-frontend-virid.vercel.app",
-    methods:["POST", "GET"],
+    origin: "https://book-app-frontend-virid.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
-}
-));
+}));
+
+// Routes
 app.use("/books", router); // localhost:5000/books
 app.use("/user", router2); // localhost:5000/user
 
 mongoose.connect(
     "mongodb+srv://foodApp123:foodApp123@cluster0.txvgoyc.mongodb.net/bookStore?retryWrites=true&w=majority"
 )
-    .then(() => console.log("connected to Database"))
     .then(() => {
-        app.listen(5000);
+        console.log("Connected to Database");
+        app.listen(5000, () => {
+            console.log("Server is running on port 5000");
+        });
     })
-    .catch((err) => console.log("Error connecting to Database"))
+    .catch((err) => console.log("Error connecting to Database", err));
